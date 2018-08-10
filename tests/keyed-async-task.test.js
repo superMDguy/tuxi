7896cee910131ba53b218d203dc233e5911b43bd
Matthew Dangerfield <matthew.dangerfield@yandex.com>
2018-08-10 14:54:37 -0500

    add rest of unit tests

--- tests/keyed-async-task.test.js
+++ tests/keyed-async-task.test.js
@@ -1,6 +1,8 @@
 import tuxi from '../lib'
 import helpers from './helpers'
 
+const payload = { name: 'bob' }
+
 test('Tuxi imported correctly', () => {
   expect(tuxi).toEqual(expect.anything())
   expect(tuxi.keyed).toEqual(expect.anything())
@@ -10,16 +12,16 @@ test('Tuxi imported correctly', () => {
 test('Supports string for key', async () => {
   const RESULT = 'async result'
   const task = tuxi.keyed(helpers.asyncTimeout(50, RESULT), 'name')
-  const payload = { name: 'bob' }
   await task.start(payload)
+  expect(task.hasValue(payload)).toEqual(true)
   expect(task.value(payload)).toEqual(RESULT)
 })
 
 test('Supports function for key', async () => {
   const RESULT = 'async result'
   const task = tuxi.keyed(helpers.asyncTimeout(50, RESULT), helpers.stringify)
-  const payload = { name: 'bob' }
   await task.start(payload)
+  expect(task.hasValue(payload)).toEqual(true)
   expect(task.value(payload)).toEqual(RESULT)
 })
 
@@ -27,7 +29,6 @@ test('Initial value and clear', async () => {
   const task = tuxi.keyed(helpers.asyncTimeout(), 'name', {
     initialValue: 'tuxi'
   })
-  const payload = { name: 'bob' }
 
   const taskPromise = task.start(payload)
   expect(task.value(payload)).toBe('tuxi')
@@ -38,104 +39,64 @@ test('Initial value and clear', async () => {
 
 test('Pending', async () => {
   const task = tuxi.keyed(helpers.asyncTimeout(), 'name')
-  const payload = { name: 'bob' }
-
   const taskPromise = task.start(payload)
   expect(task.pending(payload)).toBe(true)
   await taskPromise
   expect(task.pending(payload)).toBe(false)
 })
 
-// test('Spinner delay', async () => {
-//   const task = tuxi.task(helpers.asyncTimeout(500), {
-//     spinnerDelay: 250
-//   })
+test('Spinner delay', async () => {
+  const task = tuxi.keyed(helpers.asyncTimeout(500), 'name', {
+    spinnerDelay: 250
+  })
 
-//   const taskDone = task.start()
+  const taskPromise = task.start(payload)
 
-//   setTimeout(() => {
-//     expect(task.spinning).toBe(true)
-//   }, 300)
+  setTimeout(() => {
+    expect(task.spinning(payload)).toBe(true)
+  }, 300)
 
-//   await taskDone
-//   expect(task.spinning).toBe(false)
+  await taskPromise
+  expect(task.spinning(payload)).toBe(false)
+})
 
-//   const longerTask = tuxi.task(helpers.asyncTimeout(750), {
-//     spinnerDelay: 500
-//   })
+test('Error if key is not defined', () => {
+  const task = tuxi.keyed(helpers.asyncTimeout(500), 'name')
 
-//   const longerTaskDone = longerTask.start()
+  expect.assertions(1)
+  try {
+    task.start({})
+  } catch (err) {
+    expect(err).toBeDefined()
+  }
+})
 
-//   setTimeout(() => {
-//     expect(longerTask.spinning).toBe(false)
-//   }, 350)
-//   setTimeout(() => {
-//     expect(longerTask.spinning).toBe(true)
-//   }, 550)
+test('Error state', async () => {
+  const ERROR_MESSAGE = 'async failure'
+  const task = tuxi.keyed(
+    () => Promise.reject(new Error(ERROR_MESSAGE)),
+    'name'
+  )
 
-//   await longerTaskDone
-//   expect(longerTask.spinning).toBe(false)
-// })
+  try {
+    await task.start(payload)
+  } catch (err) {
+    expect(err.message).toBe(ERROR_MESSAGE)
+    expect(task.error(payload)).toEqual(err)
+    expect(task.error(payload).message).toBe(ERROR_MESSAGE)
+    expect(task.hasValue(payload)).toBe(false)
+  }
+})
 
-// test('Error state', async () => {
-//   const ERROR_MESSAGE = 'async failure'
-//   const task = tuxi.task(() => Promise.reject(new Error(ERROR_MESSAGE)))
+test('Custom empty state', async () => {
+  const EMPTY_VAL = 'empty'
 
-//   expect(task.error).toBe(false)
-//   try {
-//     await task.start()
-//   } catch (err) {
-//     expect(err.message).toBe(ERROR_MESSAGE)
-//     expect(task.error).toEqual(err)
-//     expect(task.error.message).toBe(ERROR_MESSAGE)
-//     expect(task.hasValue).toBe(false)
-//   }
-// })
+  const task = tuxi.keyed(helpers.asyncTimeout(100, EMPTY_VAL), 'name', {
+    fnIsEmpty: val => val === EMPTY_VAL
+  })
 
-// test('Gets correct value', async () => {
-//   const RESULT = 'async result'
+  await task.start(payload)
 
-//   const task = tuxi.task(helpers.asyncTimeout(100, RESULT))
-
-//   expect(task.hasValue).toBe(false)
-//   await task.start()
-//   expect(task.hasValue).toBe(true)
-//   expect(task.value).toBe(RESULT)
-// })
-
-// test('Prioritizes newest call', async () => {
-//   const task = tuxi.task(helpers.asyncTimeout())
-
-//   const taskPromise1 = task.start({
-//     overrideTimeout: 500,
-//     overrideResolveWith: '1'
-//   })
-//   task.start({
-//     overrideTimeout: 100,
-//     overrideResolveWith: '2'
-//   })
-
-//   expect.assertions(3)
-//   try {
-//     await taskPromise1
-//   } catch (err) {
-//     expect(task.error).toBe(false)
-//     expect(task.hasValue).toBe(true)
-//     expect(task.value).toBe('2')
-//   }
-// })
-
-// test('Custom empty state', async () => {
-//   const EMPTY_VAL = 'empty'
-
-//   const task = tuxi.task(helpers.asyncTimeout(100, EMPTY_VAL), {
-//     fnIsEmpty: val => val === EMPTY_VAL
-//   })
-
-//   expect(task.empty).toBe(false)
-
-//   await task.start()
-
-//   expect(task.empty).toBe(true)
-//   expect(task.hasValue).toBe(false)
-// })
+  expect(task.empty(payload)).toBe(true)
+  expect(task.hasValue(payload)).toBe(false)
+})
