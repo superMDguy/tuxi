bd22c7a53a757f9b115dad6fe7c889cc63e5ca45
Matthew Dangerfield <matthew.dangerfield@yandex.com>
2018-10-10 22:55:06 -0600

    make umd build minified

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,27 @@
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
-import builtins from 'rollup-plugin-node-builtins'
+import { terser } from 'rollup-plugin-terser'
 
-export default {
-  input: 'lib/index.js',
-  output: [
-    {
-      file: 'dist/bundle.umd.js',
-      name: 'async-task',
-      format: 'umd'
+export default [
+  {
+    input: 'lib/index.js',
+    output: {
+      format: 'umd',
+      name: 'tuxi',
+      file: 'dist/tuxi.min.js',
+      globals: { util: 'util', vue: 'Vue' }
     },
-
-    {
-      file: 'dist/bundle.es.js',
-      format: 'es'
-    }
-  ],
-  plugins: [resolve(), commonjs(), builtins()],
-  external: ['vue']
-}
+    plugins: [resolve(), commonjs(), terser()],
+    external: ['vue', 'util']
+  },
+  {
+    input: 'lib/index.js',
+    output: {
+      format: 'es',
+      file: 'dist/tuxi.js',
+      globals: { util: 'util', vue: 'Vue' }
+    },
+    plugins: [resolve(), commonjs()],
+    external: ['vue', 'util']
+  }
+]
