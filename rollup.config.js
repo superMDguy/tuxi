5cde3bf316b7ca20368ee744ce0f218a39073a25
Matthew Dangerfield <matthew.dangerfield@yandex.com>
2018-10-11 16:07:12 -0600

    finalize changes

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,61 @@
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
+import babel from 'rollup-plugin-babel'
 import { terser } from 'rollup-plugin-terser'
 
 export default [
+  // Browser Build: IIFE + Babel + Minification
   {
     input: 'lib/index.js',
     output: {
-      format: 'umd',
+      format: 'iife',
       name: 'tuxi',
       file: 'dist/tuxi.min.js',
-      globals: { util: 'util', vue: 'Vue' }
+      sourceMap: true,
+      globals: { vue: 'Vue' }
     },
-    plugins: [resolve(), commonjs(), terser()],
-    external: ['vue', 'util']
+    plugins: [
+      resolve(),
+      commonjs(),
+      babel({
+        exclude: 'node_modules/**',
+        babelrc: false,
+        plugins: ['@babel/plugin-proposal-object-rest-spread']
+      }),
+      terser()
+    ],
+    external: ['vue']
   },
+
+  // NodeJS Build: CJS + babel (object rest spread only available since node v8.3)
+  {
+    input: 'lib/index.js',
+    output: {
+      format: 'cjs',
+      file: 'dist/index.js',
+      globals: { vue: 'Vue' }
+    },
+    plugins: [
+      resolve(),
+      commonjs(),
+      babel({
+        exclude: 'node_modules/**',
+        babelrc: false,
+        plugins: ['@babel/plugin-proposal-object-rest-spread']
+      })
+    ],
+    external: ['vue']
+  },
+
+  // ES Build
   {
     input: 'lib/index.js',
     output: {
       format: 'es',
-      file: 'dist/tuxi.js',
-      globals: { util: 'util', vue: 'Vue' }
+      file: 'dist/tuxi.es.js',
+      globals: { vue: 'Vue' }
     },
     plugins: [resolve(), commonjs()],
-    external: ['vue', 'util']
+    external: ['vue']
   }
 ]
